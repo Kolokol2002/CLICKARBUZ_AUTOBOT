a2bb3efd06b226cc22b2c3cc5f462f5afec60e8f
Maksym Karalash <Maks.karalash@gmail.com>
2023-04-14 23:32:51 +0300

    task-3 DONE

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  let [delay, step, amount] = e.target;
+
+  amount = Number(amount.value);
+  delay = Number(delay.value);
+  step = Number(step.value);
+
+  for (let i = 0; i < amount; i++) {
+    const resDelay = delay + step * i;
+    createPromise(i + 1, resDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((res, rej) => {
+    const random = Math.random();
+    setTimeout(() => {
+      if (random > 0.3) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
 }
